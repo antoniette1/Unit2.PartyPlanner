df8569dc088691b809865bbc99919fee6e72b22b
antoniette1 <antoniette.j.moore@gmail.com>
2023-11-02 15:19:15 -0700

    Added JS and HTML

---
+++ index.js
@@ -0,0 +1,5 @@
+// Use fetch to get party data from the API
+// Make the delete button work -- delete from the API
+// Make the form work -- post parties to the API 
+// Render the DOM with data from the state 
+// The data stored in the state is updated to stay in sync with the API 
